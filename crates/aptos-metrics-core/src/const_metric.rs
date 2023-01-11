c40fd0829d4b1e522b9cf3846110daa02f38b641
lemonxah <lemon.xah@gmail.com>
2023-01-11 15:25:05 +0200

    fixed Vec type to protobuf type

--- crates/aptos-metrics-core/src/const_metric.rs
+++ crates/aptos-metrics-core/src/const_metric.rs
@@ -37,7 +37,7 @@ impl ConstMetric {
 
         let mut metric = Metric::default();
         metric.set_counter(counter);
-        metric.set_label(labels);
+        metric.set_label(labels.into());
 
         Ok(ConstMetric {
             desc,
@@ -63,7 +63,7 @@ impl ConstMetric {
 
         let mut metric = Metric::default();
         metric.set_gauge(guage);
-        metric.set_label(labels);
+        metric.set_label(labels.into());
 
         Ok(ConstMetric {
             desc,
